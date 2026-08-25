1af545e3e4914631a2a06562c350864efe136b16
yosdwi <150526953+yosdwi@users.noreply.github.com>
2026-08-25 17:59:12 +0700

    fix(v4): isolate golden preview from legacy global styles

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
-import './index.css';
-import { ThemeProvider } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
-import theme from './theme/theme.js';
-
+import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <ThemeProvider theme={theme}>
-    <CssBaseline />
+  <React.StrictMode>
     <App />
-  </ThemeProvider>
+  </React.StrictMode>,
 );
